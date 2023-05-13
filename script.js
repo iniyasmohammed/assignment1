d1f9713278640155571632973645a4dcc00ca341
iniyasmohammed <iniyasmohammed@gmail.com>
2023-05-13 16:19:16 +0300

    assignment completed

---
+++ script.js
@@ -0,0 +1,42 @@
+//operators assignment No: 1
+//addition
+let a = 23;
+let b = 34;
+console.log(a+b);
+//substraction
+let c = 55;
+let d = 36;
+console.log(c-d);
+//division
+let e = 15;
+let f = 5;
+console.log(e/f);
+//multiplication
+let g = 34;
+let h = 25;
+let i = g*h;
+console.log(i);
+//mode
+let j = 17;
+let k = 3;
+console.log(j%k);
+//mixed operations
+let l = 45;
+let m = 34;
+let n = 23;
+let o = 3;
+
+console.log(l+m*n-o);
+//if operator example.
+let sub1 = 70;
+let sub2 = 80;
+let sub3 = 90;
+let sub4 = 50;
+
+let total = sub1+sub2+sub3+sub4;
+
+if(total>250){
+    console.log("Congrats, you are passed");
+}else{
+    console.log("better luck next time");
+}
